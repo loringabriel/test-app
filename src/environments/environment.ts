55c76b1132535b6a670eb247f15cb5652a8916ab
Lorin Muller <lorin.muller@vspartners.us>
2021-02-21 09:28:41 +0200

    second commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDuk46HvUote1SkHMzLQdKRuSqCQTAjcJM",
+    authDomain: "test-application-3f1f4.firebaseapp.com",
+    projectId: "test-application-3f1f4",
+    storageBucket: "test-application-3f1f4.appspot.com",
+    messagingSenderId: "299561017688",
+    appId: "1:299561017688:web:f0d2c40fa90b99fdec052d",
+    measurementId: "G-CYNVX3SVR6"
+  }
 };
 
 /*
